f6f1537f65516172653990bbb30e06cebb5d6c7c
Backupfridge <101788144+Backupfridge@users.noreply.github.com>
2022-03-17 17:20:30 +1100

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo ""
+./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh
+echo ""
+echo "Random passwords generated!"
+echo ""
+#bash
+#tested on fedora and ubuntu. this "password generator" will fall into the "other" folder on github cause it works on both platforms :D
+
