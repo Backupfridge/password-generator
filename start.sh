77a15c19fcf8aa6786654f3e08c4bf18cd7b95d9
Shahan Arshad <68821506+sazk07@users.noreply.github.com>
2022-03-17 12:05:52 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+read -p 'Enter password length: ' PASS_LENGTH
+for ITERABLE in {1..$PASS_LENGTH}
+do
+    cat ./gs.sh :PASSWORD
+done
+echo $PASSWORD
 
-echo ""
-./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh && ./gs.sh
-echo ""
 echo "Random passwords generated!"
-echo ""
+
 #bash
 #tested on fedora and ubuntu. this "password generator" will fall into the "other" folder on github cause it works on both platforms :D
 
