fb9ba57d02f66e416b8ee3cb84c95bc76f130c4f
Backupfridge <101788144+Backupfridge@users.noreply.github.com>
2022-04-16 22:11:18 +1000

    Update gs.sh

--- gs.sh
+++ gs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-NEW_UUID_MORE_CHARACTERS=$(cat /dev/urandom | tr -dc "a-zA-Z0-9!@#$%^&()_+?><~\;" | fold -w 32 | head -n 1)
+NEW_UUID_MORE_CHARACTERS=$(cat /dev/urandom | tr -dc "a-zA-Z0-9!@#$%^&()_+?><~\;" | fold -w 50 | head -n 1)
 echo $NEW_UUID_MORE_CHARACTERS
